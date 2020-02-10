e1005e8f1a643c63dcd8a57548c9422de1ddbbb9
chetan4635 <chetan.singh@mail.vinove.com>
2020-02-11 01:36:14 +0530

    First Commit

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+var favicon = require('serve-favicon')
 const app = express();
-
+app.use(favicon(path.join(__dirname, 'public/images', 'loc.png')))
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/public'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/public/index.html'));
+res.send(path.join(__dirname+'/public/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
