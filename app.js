f1d21a7e6dba86d372255da550fffe15e7cc3d78
chetan4635 <chetan.singh@mail.vinove.com>
2020-02-16 16:44:27 +0530

    Meta Data Modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 var favicon = require('serve-favicon')
 const app = express();
-app.use(favicon(path.join(__dirname, 'public/images', 'loc.png')))
+app.use(favicon(path.join(__dirname, 'public/images', 'logo.jpg')))
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/public'));
 
